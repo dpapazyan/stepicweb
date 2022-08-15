89e2802d0676f1a47d9be2560e77816758934bab
dpapazyan <dpapazyan@gmail.com>
2022-08-15 16:43:58 +0300

    10

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/shit.conf
 sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/shit
+gunicorn -b :8080 hello:app
