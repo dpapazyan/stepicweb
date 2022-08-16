8205374aaa6e746d404e06cb9667fe7e9140a305
dpapazyan <dpapazyan@gmail.com>
2022-08-16 14:59:33 +0300

    12

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def divide_params(environ, start_response):
+def app(environ, start_response):
     body = [bytes(e + '\n', encoding='ascii') for e in environ['QUERY_STRING'].split(sep='&')]
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/shit.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/shit
-gunicorn -b :8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello:app
